f65865a1a09112480c00111ff72b76b846f08a97
bodhi <bodwang@deloitte.com.cn>
2019-07-01 13:26:20 +0800

    modify the codes to retrieve list from file hierarchy.yml and select items.

--- crawlers/crawler.py
+++ crawlers/crawler.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import os
 import sys
 import hashlib
@@ -5,6 +6,9 @@ import time
 
 from image_crawler import ImageCrawler
 from image_downloader import ImageDownloader
+
+from pprint import pprint
+import re
 
 SEP = '-' * 70 + '\n'
 n_scroll = 2
@@ -64,20 +68,29 @@ if __name__ == "__main__":
 
     if len(sys.argv)<3:
         print("Usage: python3.x crawler.py startLine endLine")
-        exit()
-    startLine, endLine = int(sys.argv[1])-1, int(sys.argv[2])-1
+        startLine = 1
+        endLine = 5
+    else:
+        startLine, endLine = int(sys.argv[1])-1, int(sys.argv[2])-1
 
     hierachy = os.path.join('../hierarchy', 'hierarchy.yml')
-    f = open(hierachy, "r")
+    '''
+    f = open(hierachy, encoding="utf-8", errors="surrogateescape")
     lines = f.read().split("\n")
     f.close()
+    '''
+    with open(hierachy, encoding="utf-8", errors="surrogateescape") as f:
+        lines = f.read ().split ("\n")
+
+    lines = [line.strip() for line in lines if re.search(r'^      \s*\w+[^类]$', line)]
+    #pprint(lines)
+    #sys.exit(0)
+
     for i in range(len(lines)):
         if startLine <= i and i<=endLine:
             keyword = lines[i]
-            if len(keyword) > 6 and keyword[:6] == "   "*2 and not keyword.endswith("类"):
-                keyword = keyword.strip()
-                print("crawl: "+keyword)
-                crawl(keyword, n_scroll, 'baidu')
-                print("sleep: 10")
-                time.sleep(10)
+            print("crawl: "+keyword)
+            crawl(keyword, n_scroll, 'baidu')
+            print("sleep: 10")
+            time.sleep(10)
 
